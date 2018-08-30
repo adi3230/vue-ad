ae9df4d840c0ec1da05f83c4ead40ac19b662252
Aditya Joshi <adi3230@gmail.com>
2018-08-30 23:51:18 +0200

    chore(): Initial setup of firebase, vue router, store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { store} from './store'
+
+const fb = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+// handle page reloads
+/* eslint-disable */
+let app
+fb.auth.onAuthStateChanged(user => { 
+    if (!app) {
+        app = new Vue({
+            el: '#app',
+            router,
+            store,
+            render: h => h(App)
+        })
+    }
+})
