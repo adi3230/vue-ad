f405a74bb4569bd7c98bc880827e7db3a72aaef4
Aditya Joshi <aditya.joshi@diconium.com>
2018-08-31 11:45:59 +0200

    feat(login): basic template and create actions, mutations for the login

--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { stat } from 'fs';
 
 Vue.use(Vuex)
 
+const fb = require('./firebaseConfig');
+
 export const store = new Vuex.Store({
     state: {
-        
+		currentUser: null,
+		userProfile: {}
     },
     actions: {
-        
+        fetchUserProfile({commit, state}) {
+			fb.usersCollection.doc(state.currentUser.uid).get().then(res => {
+				commit('setUserProfile', res.date())
+			})
+			/* eslint-disable */
+			.catch(err => console.log(err))
+		}
     }, 
     mutations: {
-        
+        setCurrentUser(state, val) {
+			state.currentUser = val
+		},
+		setUserProfile(state, val) {
+			state.userProfile = val
+		}
     }
 })
